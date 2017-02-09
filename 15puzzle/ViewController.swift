7b9de3bbe6065eab3288b33511f5ac2705ce817b
Jasmine D Emerson <jazafras@ecs325-12.labs.encs>
2017-02-09 09:44:40 -0800

    Initial Commit

---
+++ 15puzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  15puzzle
+//
+//  Created by Jasmine D Emerson on 2/9/17.
+//  Copyright © 2017 Jasmine D Emerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
